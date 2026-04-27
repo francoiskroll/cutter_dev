178ebd58528498bff2d865bae089986dfcaa4a86
Francois Kroll <francois@kroll.be>
2026-04-27 22:19:45 +0200

    simulateIns.R: now extendNewlySeq=0 & updated exponential's parameter. Can also handle be being given data from multiple loci.

--- simulateIns.R
+++ simulateIns.R
@@ -20,108 +20,138 @@
 # simulateIns -------------------------------------------------------------
 
 # ! assumes that mut was already ran through detectTemplatedIns
+# 27/01/2026 fit_exprate=0.06643421 > 0.1106152573
+# default extendNewlySeq=0
+# should probably delete parameter!
+
+# 12/02/2026
+# can now handle multiple loci
 
 simulateIns <- function(mutdti,
                         nreads=1000,
-                        extendNewlySeq=3,
+                        extendNewlySeq=0,
                         searchWindowStarts=20,
                         minLCSbp=5,
                         fit_exprate=0.06643421) {
   
-  # ! can only simulate a sample for *one* locus
-  # check that we only have one locus
-  if(length(unique(mutdti$locus))>1) stop('\t \t \t \t Error simulateIns: can only simulate a sample for *one* locus at a time.\n')
+  # loop through loci in mut
+  loci <- unique(mutdti$locus)
   
-  ### get overall reference sequence
-  # we simply remove the - hyphens
-  orefs <- gsub('-', '', mutdti$ref)
-  
-  # check that all the same
-  oref <- unique(orefs)
-  
-  if(length(oref)>1)
-    stop('\t \t \t \t >>> Error simulateIns: more than one reference sequence in this mutation table.\n')
-  
-  # get cutpos
-  cutpos <- unique(mutdti$cutpos)
-  # check only one
-  if(length(cutpos)>1)
-    stop('\t \t \t \t >>> Error simulateIns: more than one cutpos in this mutation table.\n')
-  
-  ### now simulate `nreads` newly synthesised sequences
-  # step below is surprisingly slow...
-  siminsL <- lapply(1:nreads, function(i) {
-    cat('\t \t \t \t simulating newly synthesised sequence', i, 'of', nreads, '\n')
-    simulateIns_one(extendNewlySeq=extendNewlySeq,
-                    searchWindowStarts=searchWindowStarts,
-                    minLCSbp=minLCSbp,
-                    cutpos=cutpos,
-                    oref=oref,
-                    fit_exprate=fit_exprate)
-  })
-  simins <- data.frame(do.call(rbind, siminsL))
-  # >> we get a dataframe with columns newlyseq, newlyseq_bp, etc
-  # including the LCS etc.
-  # as in detectTemplatedIns
-  
-  ### add rid
-  # normally done by alleleToMutation
-  # here, will simply be r1.1, r2.1, r3.1, etc.
-  # until e.g. r1000.1 if nreads=1000
-  simins$rid <- sprintf('r%i.1', 1:nrow(simins))
-  
-  ### add coverage
-  # copying from allelesToMutations so we get the same columns
-  # calculate total number of reads for this sample, i.e. coverage
-  # and add it as column
-  # here, we know this is exactly `nreads`
-  cat('\t \t \t \t >>> Total number of reads:', nreads, '\n')
-  simins <- simins %>%
-    mutate(splcov=nreads)
-  
-  ### add rundate, well, locus, type
-  # these we know are present in mut
-  simins <- simins %>%
-    mutate(well='Z99', .before=1) %>%
-    mutate(locus='simulated', .before=1) %>%
-    mutate(rundate=999999, .before=1) %>%
-    mutate(type='ins', .before=1)
-  
-  ### also create sample column
-  # which is rundate_well_locus
-  simins <- simins %>%
-    mutate(sample=paste(rundate, well, locus, sep='_'), .before=1)
-  
-  ### add last columns as NA
-  # find the ones we are missing from mut
-  # these were added from meta
-  cols2add <- colnames(mutdti) [! colnames(mutdti) %in% colnames(simins)]
-  
-  for(colnm in cols2add) {
+  ### for each locus, simulate reads
+  siminsL <- lapply(loci, function(li) {
+    
+    cat('\t \t \t \t Simulating insertion reads for locus', li, '\n')
+    
+    # mutation table for one locus
+    mutli <- mutdti %>%
+      filter(locus==li)
+    
+    # check we only have one locus to be safe
+    if(length(unique(mutli$locus))>1) stop('\t \t \t \t Error simulateDel: can only simulate a sample for *one* locus at a time.\n')
+    
+    ### get overall reference sequence
+    # we simply remove the - hyphens
+    orefs <- gsub('-', '', mutli$ref)
+    
+    # check that all the same
+    oref <- unique(orefs)
+    
+    if(length(oref)>1)
+      stop('\t \t \t \t >>> Error simulateIns: more than one reference sequence in this mutation table.\n')
+    
+    # get cutpos
+    cutpos <- unique(mutli$cutpos)
+    # check only one
+    if(length(cutpos)>1)
+      stop('\t \t \t \t >>> Error simulateIns: more than one cutpos in this mutation table.\n')
+    
+    ### now simulate `nreads` newly synthesised sequences
+    # step below is surprisingly slow...
+    siminsL <- lapply(1:nreads, function(i) {
+      cat('\t \t \t \t simulating newly synthesised sequence', i, 'of', nreads, '\n')
+      simulateIns_one(extendNewlySeq=extendNewlySeq,
+                      searchWindowStarts=searchWindowStarts,
+                      minLCSbp=minLCSbp,
+                      cutpos=cutpos,
+                      oref=oref,
+                      fit_exprate=fit_exprate)
+    })
+    simins <- data.frame(do.call(rbind, siminsL))
+    # >> we get a dataframe with columns newlyseq, newlyseq_bp, etc
+    # including the LCS etc.
+    # as in detectTemplatedIns
+    
+    ### add rid
+    # normally done by alleleToMutation
+    # here, will simply be r1.1, r2.1, r3.1, etc.
+    # until e.g. r1000.1 if nreads=1000
+    simins$rid <- sprintf('r%i.1', 1:nrow(simins))
+    
+    ### add coverage
+    # copying from allelesToMutations so we get the same columns
+    # calculate total number of reads for this sample, i.e. coverage
+    # and add it as column
+    # here, we know this is exactly `nreads`
+    cat('\t \t \t \t >>> Total number of reads:', nreads, '\n')
     simins <- simins %>%
-      mutate(tmp=NA, .before='well')
-    # now put the correct column name
-    colnames(simins)[which(colnames(simins)=='tmp')] <- colnm
-  }
+      mutate(splcov=nreads)
+    
+    ### add rundate, well, locus, type
+    # these we know are present in mut
+    simins <- simins %>%
+      mutate(well='sim', .before=1) %>%
+      mutate(grp=paste(unique(mutli$locus), 'simulated', sep='_')) %>%
+      mutate(locus=unique(mutli$locus), .before=1) %>%
+      mutate(rundate=999999, .before=1) %>%
+      mutate(type='ins', .before=1)
+    
+    ### also create sample column
+    # which is rundate_well_locus
+    simins <- simins %>%
+      mutate(sample=paste(rundate, well, locus, sep='_'), .before=1)
+    
+    ### add last columns as NA
+    # find the ones we are missing from mut
+    # these were added from meta
+    cols2add <- colnames(mutli) [! colnames(mutli) %in% colnames(simins)]
+    
+    for(colnm in cols2add) {
+      simins <- simins %>%
+        mutate(tmp=NA, .before='well')
+      # now put the correct column name
+      colnames(simins)[which(colnames(simins)=='tmp')] <- colnm
+    }
+    
+    # refill cutpos column
+    simins$cutpos <- cutpos
+    
+    return(simins)
+    # simins gets added to list simdelL
+    
+    
+  })
+  # closes lapply
+  # we now have one simdel dataframe per locus
+  # stored in a list
   
-  # refill cutpos column
-  simins$cutpos <- cutpos
+  # rbind the list (we can keep track of locus through locus column)
+  simdf <- do.call(rbind, siminsL)
   
   ### add simulated reads to mut
   # first put the columns of simins in the same order as in mut
   
   # check columns are identical (before looking at their positions)
-  if(!identical( sort(colnames(mutdti)) , sort(colnames(simins))))
-    stop('\t \t \t \t >>> Error simulateIns: all columns of mut are not in simins, or vice-versa.\n')
+  if(!identical( sort(colnames(mutdti)) , sort(colnames(simdf))))
+    stop('\t \t \t \t >>> Error simulateIns: all columns of mut are not in simdf, or vice-versa.\n')
   
   # now put the columns of simins in the same order
-  simins <- simins[, colnames(mutdti) ]
+  simdf <- simdf[, colnames(mutdti) ]
   # check correct
-  if(!identical(colnames(mutdti), colnames(simins)))
-    stop('\t \t \t \t >>> Error simulateIns: columns of simins are not in the same order as columns of mut.\n')
+  if(!identical(colnames(mutdti), colnames(simdf)))
+    stop('\t \t \t \t >>> Error simulateIns: columns of simdf are not in the same order as columns of mut.\n')
   
   # can now rbind
-  mutsim <- rbind(mutdti, simins)
+  mutsim <- rbind(mutdti, simdf)
   # and we return result
   return(mutsim)
   
